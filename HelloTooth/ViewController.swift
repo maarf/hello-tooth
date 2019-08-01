64599a00a4f945711d8a4dd962defd364cbabe88
Martins Spilners <martins@ctrld.ch>
2019-08-01 20:17:50 +0300

    Initial commit

---
+++ HelloTooth/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  HelloTooth
+//
+//  Created by Martins on 01/08/2019.
+//  Copyright © 2019 Good Gets Better. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewWillAppear(_ animated: Bool) {
+    
+  }
+}
